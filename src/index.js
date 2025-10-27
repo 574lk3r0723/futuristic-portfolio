5b955ee1c55e66ebb72f550b254b9189048e7b35
Ryan Boulton <ryanb0723@proton.me>
2025-10-27 10:26:04 -0400

    Add index.html to public directory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import PortfolioApp from "./PortfolioApp";
-import "./index.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<PortfolioApp />);
-
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
